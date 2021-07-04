f771ebb4ca912f8299b32d997043e75c480e39f0
Artem <artem30801@gmail.com>
2021-07-04 17:17:10 +0300

    Added basic setup requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+extras = {
+    "lint": ["black", "flake8", "isort"],
+}
+
+setuptools.setup(
+    name="clover_swarm",
+    author="Artem Vasiunik",
+    packages=setuptools.find_packages(),
+    extras_require=extras,
+    python_requires=">=3.6",
+)
